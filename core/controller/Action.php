9d3d394200b9fa5eb20a00a21576e50132c5a29d
efarfico <efarfico@users.noreply.github.com>
2017-08-10 12:45:06 -0500

    Sistema
    Sistema de inventario PHP

---
+++ core/controller/Action.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class Action {
+	
+	public static function load($action){
+		// Module::$module;
+		
+		if(!isset($_GET['action'])){
+			include "core/modules/".Module::$module."/action/".$action."/action-default.php";
+		}else{
+
+
+			if(Action::isValid()){
+				include "core/modules/".Module::$module."/action/".$_GET['action']."/action-default.php";				
+			}else{
+				Action::Error("<b>404 NOT FOUND</b> Action <b>".$_GET['action']."</b> folder  !!");
+			}
+
+
+
+		}
+	}
+
+		public static function isValid(){
+		$valid=false;
+		if(file_exists($file = "core/modules/".Module::$module."/action/".$_GET['action']."/action-default.php")){
+			$valid = true;
+		}
+		return $valid;
+	}
+
+	public static function Error($message){
+		print $message;
+	}
+
+	public function execute($action,$params){
+		$fullpath =  "core/modules/".Module::$module."/action/".$action."/action-default.php";
+		if(file_exists($fullpath)){
+			include $fullpath;
+		}else{
+			assert("wtf");
+		}
+	}
+
+}
+
+
+
+?>
